cfd8419ae80df128b83917a82f89e1747712efd4
Jakub Kaderka <jakub.kaderka@gmail.com>
2022-06-01 08:38:35 +0200

    Converted examples to python3

--- examples/example4/example4.py
+++ examples/example4/example4.py
@@ -10,10 +10,10 @@ v3 = latexexpr.Variable('v3',1.23)
 v4 = latexexpr.Variable('v4',4.56)
 
 def printExpr(e1,e2=''):
-	print '$$' + str(e1) + r'\ \ \ \ \ \ \ \ \ ' + str(e2) + '$$\n'
+	print('$$' + str(e1) + r'\ \ \ \ \ \ \ \ \ ' + str(e2) + '$$\n')
 
 # simplify
-print '\n\nsimplify'
+print('\n\nsimplify')
 e1 = latexexpr.Expression('e1',v1+v1+v2+v3+v2+v3-v4)
 printExpr( e1, lsympy.simplify(e1) )
 printExpr( lsympy.simplify(e1) )
@@ -23,7 +23,7 @@ e3 = latexexpr.Expression('e3', (x**3 + x**2 - x - 1) / (x**2 + 2*x + 1) )
 printExpr( e3, lsympy.simplify(e3) )
 
 # expand
-print '\n\nexpand'
+print('\n\nexpand')
 e1 = latexexpr.Expression('e1', (x+1)**2 )
 printExpr( e1, lsympy.expand(e1,substituteFloats=True) )
 e2 = latexexpr.Expression('e2', (x+2)*(x-3) )
@@ -32,19 +32,19 @@ e3 = latexexpr.Expression('e3', (x+1)*(x-2) - (x-1)*x )
 printExpr( e3, lsympy.expand(e3) )
 
 # factor
-print '\n\nfactor'
+print('\n\nfactor')
 e1 = latexexpr.Expression('e1', x**3 - x**2 + x - 1)
 printExpr( e1, lsympy.factor(e1) )
 e2 = latexexpr.Expression('e2', x**2*z + 4*x*y*z + 4*y**2*z)
 printExpr( e2, lsympy.factor(e2) )
 
 # collect
-print '\n\ncollect'
+print('\n\ncollect')
 e1 = latexexpr.Expression('e1', x*y + x - 3  + 2*x**2 - z*x**2 + x**3)
 printExpr( e1, lsympy.collect(e1,x) )
 
 # cancel
-print '\n\ncancel'
+print('\n\ncancel')
 e1 = latexexpr.Expression('e1', (x**2 + 2*x + 1) / (x**2 + x) )
 printExpr( e1, lsympy.cancel(e1) )
 e2 = latexexpr.Expression('e2', 1/x + (3*x/2 - 2) / (x - 4) )
@@ -53,6 +53,6 @@ e3 = latexexpr.Expression('e3', (x*y**2 - 2*x*y*z + x*z**2 + y**2 - 2*y*z + z**2
 printExpr( e3, lsympy.cancel(e3) )
 
 # apart
-print '\n\napart'
+print('\n\napart')
 e1 = latexexpr.Expression('e1', (4*x**3 + 21*x**2 + 10*x + 12) / (x**4 + 5*x**3 + 5*x**2 + 4*x) )
 printExpr( e1, lsympy.apart(e1) )
